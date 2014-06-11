5b5f36ef1ac653a649bf5f6a96597393e9be6659
Lohrr <Lohrr2k@yahoo.com>
2014-06-11 04:55:00 +0000

    Stopped allowing tracked artis given as quest rewards. In other words, if an arti is already on a char (belt buckle named Rage), then it can not be given out as a quest reward (duplicating the arti).

--- src/quest.c
+++ src/quest.c
@@ -232,6 +232,15 @@ void give_reward(struct quest_complete_data *qcp, P_char mob, P_char pl)
     {
     case QUEST_GOAL_ITEM:
       obj = read_object(gp->number, VIRTUAL);
+
+      // No duplicating artifacts.
+      if( IS_ARTIFACT(obj) && get_current_artifact_info(obj->R_num, 0, NULL, NULL, NULL, NULL, FALSE, NULL) )
+      {
+        // It's imperative that we extract.. FALSE because otherwise extract_obj kills artifact info.
+        extract_obj(obj, FALSE);
+        break;
+      }
+
       if (!obj)
       {
         logit(LOG_DEBUG, "give_reward(): obj %d not loadable", gp->number);
