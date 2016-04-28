3e4ac2b3215084d44847c9f04c38bf19674ee458
Lohrr <lohrr@durismud.com>
2016-04-28 22:52:54 +0000

    Removed old Players/Artifacts directories & files.  Updated backup_pfiles.sh to use seconds-only format for the backup-directory name instead of a Y.M.D-H.M.S format.  This made it easy to auto-remove all backups that are over a week old (done each boot).  Also, added backing-up of Ships, Corpses, Justice, SavedItems, Kingdoms, ShopKeepers, Shapechange, Tradeskills and House directories and contents.

--- backup_pfiles.sh
+++ backup_pfiles.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
 
-DATESTR=`date +%C%y.%m.%d-%H.%M.%S`
+DATESTR_FULL=`date +%C%y.%m.%d-%H.%M.%S`
+DATESTR=`date +%s`
+#This is the oldest allowed directory name in Players/Backup
+#  It is equivalent to 1 week ago (7days * 24hrs *60 mins * 60secs)
+OLDDATES=`expr $DATESTR - 7 \* 24 \* 60 \* 60`
 
+FILENAMES=`ls Players/Backup/`
+FILENAMES_LENGTH=${#FILENAME}
+
+#If there are any files to possibly remove...
+if [[ `expr $FILENAMES_LENGTH \> 0` ]]; then
+  #Look through them and remove them if they're old.
+  for WORD in $FILENAMES; do
+    if [[ $WORD < $OLDDATES ]]; then
+      DATE=`date -d @${WORD}`
+      echo "Removing old backup Players/Backup/$WORD = $DATE"
+      rm -r Players/Backup/$WORD
+    fi
+  done
+fi
+
+echo "Creating backup directory: Players/Backup/$DATESTR = $DATESTR_FULL"
 mkdir Players/Backup/$DATESTR
 
+#We have to do this manually since there are other directories & files in Players/
 mkdir Players/Backup/$DATESTR/a
   cp Players/a/* Players/Backup/$DATESTR/a 2>/dev/null
 mkdir Players/Backup/$DATESTR/b
@@ -56,3 +77,26 @@ mkdir Players/Backup/$DATESTR/y
   cp Players/y/* Players/Backup/$DATESTR/y 2>/dev/null
 mkdir Players/Backup/$DATESTR/z
   cp Players/z/* Players/Backup/$DATESTR/z 2>/dev/null
+
+#These just have a few files in the directory.
+#Note: Ships is not in the Players/ directory, but we still want to back it up.
+mkdir Players/Backup/$DATESTR/Ships
+  cp Ships/* Players/Backup/$DATESTR/Ships 2>/dev/null
+mkdir Players/Backup/$DATESTR/Corpses
+  cp Players/Corpses/* Players/Backup/$DATESTR/Corpses 2>/dev/null
+mkdir Players/Backup/$DATESTR/Justice
+  cp Players/Justice/* Players/Backup/$DATESTR/Justice 2>/dev/null
+mkdir Players/Backup/$DATESTR/SavedItems
+  cp Players/SavedItems/* Players/Backup/$DATESTR/SavedItems 2>/dev/null
+mkdir Players/Backup/$DATESTR/Kingdoms
+  cp Players/Kingdoms/* Players/Backup/$DATESTR/Kingdoms 2>/dev/null
+mkdir Players/Backup/$DATESTR/ShopKeepers
+  cp Players/ShopKeepers/* Players/Backup/$DATESTR/ShopKeepers 2>/dev/null
+
+#These have subdirectories... we want all of it.
+mkdir Players/Backup/$DATESTR/Shapechange
+  cp -r Players/Shapechange/* Players/Backup/$DATESTR/Shapechange 2>/dev/null
+mkdir Players/Backup/$DATESTR/Tradeskills
+  cp -r Players/Tradeskills/* Players/Backup/$DATESTR/Tradeskills 2>/dev/null
+mkdir Players/Backup/$DATESTR/House
+  cp -r Players/House/* Players/Backup/$DATESTR/House 2>/dev/null
